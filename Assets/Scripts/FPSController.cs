0f942c00b4d3884893270f41cd61c95c1790f80b
McClauss <cmhc85@gmail.com>
2022-07-10 01:09:58 -0400

    V.0.0.2
    Manejo Texturas

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    //Player Movement
+    public float velocidadMovimiento = 3f;
+
+
+
+    //Player Look Rotation
+    public float sensibilidadMouse = 100f;
+    public float xRotacion;
+    public float yRotacion;
+    public Transform cam;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+   
+    void Update()
+    {
+      Move();  
+      MouseLook();
+    }
+
+
+    void Move()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(hor,0,ver) * velocidadMovimiento * Time.deltaTime);
+    }
+
+    void MouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") *sensibilidadMouse * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") *sensibilidadMouse * Time.deltaTime;
+
+        xRotacion -= mouseY;
+        xRotacion = Mathf.Clamp(xRotacion, -70,70);
+
+        yRotacion += mouseX;//esto es asi sino funciona al reves
+        cam.localRotation= Quaternion.Euler(xRotacion,yRotacion,0);
+    }
+}
---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+
+    int cantidadMonedas=0;
+    int sangre=100;
+
+    public float speed=2;
+
+    public Transform posEnemigo;//Referencia a Posición de Enemigo para saber proximidad
+
+    Vector3 posInicial;//Guarda posicioon inicial del jugador
+
+    public Animator anim;// Variable para referencia al animator
+
+    public AudioSource audioS;// Se usara para poner play a un sonido
+    public AudioClip clipSonido;// Al sonido que se le dara play
+
+    void Start()
+    {
+        posInicial=transform.position;
+    }
+
+    
+    void Update()
+    {
+        MovimientoJugador();
+        ChequearDistancia();
+        /*Evalua si el jugador cayo del escenario*/
+        if(transform.position.y <-10){
+            Respawn();
+        }
+        //numeroRandom=Random.Range(0,20);
+        //puerta.transform.position=new Vecor3(numeroRandom,numeroRandom,numeroRandom);
+    }
+    
+    //Función para mover jugador con Teclado (Felchas y AWSD)
+    void MovimientoJugador(){
+        float hor= Input.GetAxis("Horizontal");//Lee Eje Horizontal
+        float ver= Input.GetAxis("Vertical");//Lee Eje Vertical
+        
+        Vector3 inputPlayer = new Vector3(hor,0,ver);//Para identificar si el jugador se esta moviendo o no - Uso de Animaciones
+
+
+        //transform.Translate(new Vector3(0,0,ver)*speed*Time.deltaTime);//Para mover el jugador
+        //transform.Rotate(0,hor,0); /*Para rotar el jugador*/
+
+        //transform.Translate(new Vector3(hor,0,ver)*speed*Time.deltaTime);//Para mover y rotar al tiempo
+        
+        transform.Translate(inputPlayer*speed*Time.deltaTime);//Para mover manejando el inputPlayer
+        transform.Rotate(0,hor,0);
+
+        /*Para Manejo de Animación Caminar-Detenerse, saber si se esta oprimiendo teclas de movimiento o no*/
+        if(inputPlayer == Vector3.zero){
+            anim.SetBool("estaCaminando",false);
+            
+        }else{
+            anim.SetBool("estaCaminando",true);
+            StartAudioClip(clipSonido);
+        }
+        
+    }
+
+    /*Funcion para Calcular Distancia Contra un Objeto*/
+    void ChequearDistancia(){
+        float distancia=Vector3.Distance(transform.position,posEnemigo.position);//mide distancia entre posicion vs posicion dos
+
+        if (distancia<3){
+            Debug.Log("Enemigo Próximo: "+ distancia);
+        }
+    }
+
+    /*Metodo para Respawn al Jugador*/
+    void Respawn(){
+        transform.position=posInicial;
+    }
+
+    //Método para manejo de Sonidos
+    void StartAudioClip(AudioClip clip){
+        audioS.clip=clip;
+        audioS.Play();
+    }
+
+
+    //Método Propio de Trigger para el Tema deteccion de Coliciones Ejemplo: Manejo de Monedas
+
+    //Tambien hay OnTriggerStay (Colicion Constante) y OnTriggerExit Colicion al Salir
+    void OnTriggerEnter(Collider col){
+        
+        if(col.transform.gameObject.tag == "Moneda"){
+            cantidadMonedas++;
+            Debug.Log("He colicionado con una Moneda. "+cantidadMonedas);
+            //col.transform.gameObject.SetActive(false);
+            Destroy(col.transform.gameObject);
+        }
+
+        if(col.transform.gameObject.tag == "Pinchos"){
+            cantidadMonedas++;
+            Debug.Log("He colicionado con un Pincho. ");
+            Respawn();
+        }
+    }
+
+
+
+}
